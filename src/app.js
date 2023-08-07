80666c394471699aaba59ac60033076d8315a02a
Rick Nuñez <113080329+RickNunez@users.noreply.github.com>
2023-08-07 08:20:44 +0000

    Gen mond

--- src/app.js
+++ src/app.js
@@ -1,18 +1,1 @@
-var weekday = '';
-weekday = prompt('Qué dia de la semana es hoy');
-
-switch(weekday)
-{
-	case "Lunes":
-		console.log('Es el comienzo de una semana aburrida');
-	break;
-
-	case "Viernes":
-		console.log('De Carrete!');
-	break;
-
-	default:
-		console.log('Es otro dia aburrido....');
-	break;
-}
-
+console.log("Generator Rick");
